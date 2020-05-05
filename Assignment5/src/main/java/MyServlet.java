2016e537535ead42932fb08237085f3ce484041a
Intrejan <1157982786@qq.com>
2020-05-05 12:21:01 +0800

    by ycj,get方法完成

--- Assignment5/src/main/java/MyServlet.java
+++ Assignment5/src/main/java/MyServlet.java
@@ -1,13 +1,13 @@
 package main.java;
 
-import java.io.IOException;
+import java.io.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.PrintWriter;
+import javax.xml.namespace.QName;
+import javax.xml.soap.*;
 import java.util.Iterator;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
@@ -29,13 +29,13 @@ public class MyServlet extends HttpServlet {
     }
 
     /**
+     * 处理客户端的GET请求
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String id = request.getParameter("id");
         StringBuilder soapXML = new StringBuilder();
         String errorMessage = "";
-        StringBuilder correctMessage = new StringBuilder();
         switch (isId(id)){
             case 1:errorMessage = "学生id中只能包含数字!";
                 break;
@@ -48,6 +48,13 @@ public class MyServlet extends HttpServlet {
         try {
             document = reader.read(new File("D:\\服务端与SOA开发\\SOA\\Assignment5\\src\\main\\resources\\StudentList.xml"));
             Element root = document.getRootElement();
+
+            MessageFactory factory = MessageFactory.newInstance();
+            SOAPMessage respMsg = factory.createMessage();
+            SOAPPart soapPart = respMsg.getSOAPPart();
+            SOAPEnvelope envelope = soapPart.getEnvelope();
+
+            SOAPBody body = envelope.getBody();
             boolean hasFound=false;
             for(Iterator it = root.elementIterator(); it.hasNext();){
                 Element Student = (Element) it.next();
@@ -55,29 +62,29 @@ public class MyServlet extends HttpServlet {
                 if(Id!=null){
                     if(Id.getText().equals(id)){
                         hasFound = true;
+
+                        SOAPBodyElement bodyElement = body.addBodyElement(envelope.createName("Correct","m","http://www.w3.org/2001/XMLSchema-instance"));
+                        bodyElement.addAttribute(QName.valueOf("SOAP-ENV:encodingStyle"),"http://www.w3.org/2003/05/soap-encoding");
                         Element ScoreList = Student.element("个人成绩列表").element("课程成绩列表");
                         String sid = Id.getText();
                         String name = Student.element("学生信息").element("个人信息").element("姓名").getText();
-                        correctMessage.append("<m:Correct env:encodingStyle=\"http://www.w3.org/2003/05/soap-encoding\" xmlns:m=\"http://www.w3.org/2001/XMLSchema-instance\">\n" + "            <m:学生 学号=\"").append(sid).append("\" 姓名=\"").append(name).append("\">\n").append("                <个人成绩列表>\n");
+                        SOAPElement student = bodyElement.addChildElement(envelope.createName("学生","m","http://www.w3.org/2001/XMLSchema-instance"));
+                        student.addAttribute(QName.valueOf("学号"),sid);
+                        student.addAttribute(QName.valueOf("姓名"),name);
+                        SOAPElement scoreList = student.addChildElement(envelope.createName("个人成绩列表"));
+
                         for(Iterator iter = ScoreList.elementIterator();iter.hasNext();){
+                            Name score = envelope.createName("课程成绩");
                             Element Score = (Element)iter.next();
                             String courseId = Score.attributeValue("课程编号");
                             String scoreType = Score.attributeValue("成绩性质");
                             String scoreValue = Score.element("成绩").element("得分").getText();
-                            correctMessage.append("                    <课程成绩 课程编号=\"")
-                                    .append(courseId)
-                                    .append("\" 成绩性质=\"")
-                                    .append(scoreType)
-                                    .append("\">")
-                                    .append(scoreValue)
-                                    .append("</课程成绩>\n");
+                            scoreList.addChildElement(score).addTextNode(scoreValue).addAttribute(QName.valueOf("课程编号"),courseId).addAttribute(QName.valueOf("成绩性质"),scoreType);
                         }
-                        correctMessage.append("                </个人成绩列表>\n" +
-                                "            </m:学生>\n" +
-                                "        </m:Correct>\n");
                     }
                 }
             }
+
             if(!hasFound && errorMessage.equals("")){
                 errorMessage = "没有找到该学号对应的学生!";
             }
@@ -86,26 +93,14 @@ public class MyServlet extends HttpServlet {
             PrintWriter out = response.getWriter();
             if(errorMessage.equals("")){
                 response.setStatus(200);
-                soapXML.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-                        "<env:Envelope xmlns:env=\"http://schemas.xmlsoap.org/soap/envelope/\" >\n" +
-                        "    <env:Body>\n")
-                .append(correctMessage)
-                .append("    </env:Body>\n" +
-                        "</env:Envelope>");
-
             }else{
                 response.setStatus(500);
-                soapXML.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + "<env:Envelope xmlns:env=\"http://schemas.xmlsoap.org/soap/envelope/\" >\n" + "    <env:Body>\n" +
-                        "        <m:Fault env:encodingStyle=\"http://www.w3.org/2003/05/soap-encoding\" xmlns:m=\"http://www.w3.org/2001/XMLSchema-instance\">\n" +
-                        "            <m:errorMessage>")
-                        .append(errorMessage)
-                        .append("</m:errorMessage>\n")
-                        .append("        </m:Fault>\n")
-                        .append("    </env:Body>\n")
-                        .append("</env:Envelope>");
+                SOAPBodyElement bodyElement = body.addBodyElement(envelope.createName("Fault","m","http://www.w3.org/2001/XMLSchema-instance"));
+                bodyElement.addAttribute(QName.valueOf("SOAP-ENV:encodingStyle"),"http://www.w3.org/2003/05/soap-encoding");
+                bodyElement.addChildElement(envelope.createName("errorMessage","m","http://www.w3.org/2001/XMLSchema-instance")).addTextNode(errorMessage);
             }
-            out.println(soapXML.toString());
-        } catch (DocumentException e) {
+            out.print(soapMessageToString(respMsg));
+        } catch (DocumentException | SOAPException e) {
             e.printStackTrace();
         }
     }
@@ -132,10 +127,28 @@ public class MyServlet extends HttpServlet {
     }
 
     /**
+     * 处理客户端的POST请求
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
     }
 
+    /**
+     * 将soapMessage转化为string
+     * @param message soap消息
+     * @return string
+     */
+    public String soapMessageToString(SOAPMessage message) {
+        String result = null;
+
+        if (message != null) {
+            try (ByteArrayOutputStream bao = new ByteArrayOutputStream()) {
+                message.writeTo(bao);
+                result = bao.toString("utf-8");
+            } catch (Exception ignored) {
+            }
+        }
+        return result;
+    }
 }
